6c1c28c747e672a0340192f15f8f45890272a0a6
binbinlau <18317713525@163.com>
2019-07-08 15:17:57 +0800

    修改mybatis-generator，自定义添加注释

---
+++ server/src/test/java/binbinlau/plane/mybatis/GeneratorTest.java
@@ -0,0 +1,50 @@
+package binbinlau.plane.mybatis;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 执行该方法生成mapper、entity、 dao文件
+ * @Author LiuBin
+ * @Date 2019/7/8 13:49
+ **/
+public class GeneratorTest {
+
+    @Test
+    @Ignore
+    public void generatorTest() {
+        try {
+            List<String> warnings = new ArrayList<String>();
+            boolean overwrite = true;
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            InputStream is = classloader.getResourceAsStream("mybatis/generator-config.xml");
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(is);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- server/src/test/java/binbinlau/plane/mybatis/MariadbTest.java
+++ server/src/test/java/binbinlau/plane/mybatis/MariadbTest.java
@@ -1,8 +1,5 @@
 package binbinlau.plane.mybatis;
 
-import binbinlau.plane.xo.dao.MessageMapper;
-import binbinlau.plane.xo.entity.Message;
-import binbinlau.plane.xo.entity.MessageExample;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -22,28 +19,28 @@ import java.util.UUID;
 public class MariadbTest {
     Logger logger = LoggerFactory.getLogger(MariadbTest.class);
 
-    @Autowired
-    MessageMapper messageMapper;
-
-    @Test
-    public void testGet() {
-        MessageExample messageExample = new MessageExample();
-        messageExample.createCriteria();
-        long count = messageMapper.countByExample(messageExample);
-        logger.info("message's count is : " + count);
-    }
-
-    @Test
-    public void testInsert() {
-        Message message = new Message();
-        logger.info("uid : " + UUID.randomUUID().toString().replace("-", ""));
-        message.setUid(UUID.randomUUID().toString().replace("-", ""));
-        message.setContent("222222222222222222222222222222222");
-        message.setFromuseruid("uid11111111111111111111111111111");
-        message.setFromusername("mingming");
-        message.setStatus(true);
-        messageMapper.insert(message);
-        logger.info("message's count is : " + messageMapper.countByExample(new MessageExample()));
-    }
+//    @Autowired
+//    MessageMapper messageMapper;
+//
+//    @Test
+//    public void testGet() {
+//        MessageExample messageExample = new MessageExample();
+//        messageExample.createCriteria();
+//        long count = messageMapper.countByExample(messageExample);
+//        logger.info("message's count is : " + count);
+//    }
+//
+//    @Test
+//    public void testInsert() {
+//        Message message = new Message();
+//        logger.info("uid : " + UUID.randomUUID().toString().replace("-", ""));
+//        message.setUid(UUID.randomUUID().toString().replace("-", ""));
+//        message.setContent("222222222222222222222222222222222");
+//        message.setFromuseruid("uid11111111111111111111111111111");
+//        message.setFromusername("mingming");
+//        message.setStatus(true);
+//        messageMapper.insert(message);
+//        logger.info("message's count is : " + messageMapper.countByExample(new MessageExample()));
+//    }
 
 }
