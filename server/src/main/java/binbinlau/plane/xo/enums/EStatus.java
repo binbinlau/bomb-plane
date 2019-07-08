f6c6eed72a55e9a276f76f6a423aa803733783eb
binbinlau <18317713525@163.com>
2019-07-08 23:22:03 +0800

    fix bug

--- server/src/main/java/binbinlau/plane/xo/enums/EStatus.java
+++ server/src/main/java/binbinlau/plane/xo/enums/EStatus.java
@@ -1,4 +1,35 @@
 package binbinlau.plane.xo.enums;
 
 public enum EStatus {
+    disable("0"),
+    enable("1"),
+    deleted("2"),
+    init("10"),
+    start("11"),
+    wait("12"),
+    end("13");
+
+    private final String value;
+
+    private EStatus(String value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return this.value;
+    }
+
+    public static EStatus get(int value) {
+        String str = String.valueOf(value);
+        return get(str);
+    }
+
+    public static EStatus get(String str) {
+        for (EStatus e : values()) {
+            if (e.toString().equals(str)) {
+                return e;
+            }
+        }
+        return null;
+    }
 }
